1a6bf5d22b57a33166950132498bb3ec530990e9
matilderosa <matilderosa3@hotmail.com>
2017-06-13 15:52:59 +0000

    add databaseurl

--- app.js
+++ app.js
@@ -15,9 +15,8 @@ var express               = require("express"),
 var campgroundRoutes = require("./routes/campgrounds"),
     commentRoutes    = require("./routes/comments"),
     authRoutes       = require("./routes/index");
-    
-   
-mongoose.connect("mongodb://localhost/yelp_camp");
+
+mongoose.connect(process.env.DATABASEURL);    
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
